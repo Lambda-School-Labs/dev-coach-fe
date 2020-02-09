e4b62427d759d93b5493dcdc29ff91c876e3cbed
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-09 22:54:49 +0100

    add pagination to challenges component

--- src/views/Challenges/Challenges.js
+++ src/views/Challenges/Challenges.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uuid from 'uuid';
 import styled from 'styled-components';
+import Pagination from 'antd/lib/pagination';
 import { testDataObj } from '../../utils/challenges';
 import ChallengeCard from '../../components/Cards/ChallengeCard';
 
@@ -11,16 +12,34 @@ const StyledChallenges = styled.div`
 `;
 
 const Challenges = () => {
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(6);
+
+  const handlePagination = value => {
+    if (value <= 1) {
+      setMinValue(0);
+      setMaxValue(6);
+    } else {
+      setMinValue(value * 6 - 6);
+      setMaxValue(value * 6);
+    }
+  };
   const makeToArray = Object.values(testDataObj);
   return (
     <StyledChallenges>
-      {makeToArray.map(challenge => (
+      {makeToArray.slice(minValue, maxValue).map(challenge => (
         <ChallengeCard
           key={uuid()}
           difficulty={challenge.difficulty}
           name={challenge.name}
         />
       ))}
+      <Pagination
+        defaultCurrent={1}
+        defaultPageSize={6}
+        onChange={handlePagination}
+        total={makeToArray.length}
+      />
     </StyledChallenges>
   );
 };
