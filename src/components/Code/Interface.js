bdaa15dba689acbe82dce8cd4dd4498797a3669e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-09 04:08:51 +0100

    import challenges in interface component

--- src/components/Code/Interface.js
+++ src/components/Code/Interface.js
@@ -12,10 +12,11 @@ import {
 } from '@material-ui/core';
 import {
   mapLanguageToEditorState,
-  testDataObj,
   logCode,
   runAllCode,
 } from '../../utils/executionHelpers';
+import { testDataObj } from '../../utils/challenges';
+
 import devices from '../../utils/devices';
 
 const InterfaceContainer = styled.div`
