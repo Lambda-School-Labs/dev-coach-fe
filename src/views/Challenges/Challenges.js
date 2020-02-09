caee1472096774da3147fcca3cc5e5e4fb93140e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-09 22:10:16 +0100

    add styled compoent

--- src/views/Challenges/Challenges.js
+++ src/views/Challenges/Challenges.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import uuid from 'uuid';
+import styled from 'styled-components';
 import { testDataObj } from '../../utils/challenges';
+
+const StyledChallenges = styled.div``;
 
 const Challenges = () => {
   const array = Object.values(testDataObj);
-  return array.map(challenge => (
-    <div key={uuid()}>
-      <h2>{challenge.name}</h2>
-      <p>{challenge.difficulty}</p>
-    </div>
-  ));
+  return (
+    <StyledChallenges>
+      {array.map(challenge => (
+        <div key={uuid()}>
+          <h2>{challenge.name}</h2>
+          <p>{challenge.difficulty}</p>
+        </div>
+      ))}
+    </StyledChallenges>
+  );
 };
 
 export default Challenges;
